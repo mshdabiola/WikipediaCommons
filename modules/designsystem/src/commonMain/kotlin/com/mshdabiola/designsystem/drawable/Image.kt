215c4dabb9c73d4c8f1b828527ae7a02be5de600
mshdabiola <mshdabiola@gmail.com>
2025-05-11 09:27:19 +0100

    Rename drawable from icon to logo

--- modules/designsystem/src/commonMain/kotlin/com/mshdabiola/designsystem/drawable/Image.kt
+++ modules/designsystem/src/commonMain/kotlin/com/mshdabiola/designsystem/drawable/Image.kt
@@ -2,11 +2,11 @@ package com.mshdabiola.designsystem.drawable
 
 import androidx.compose.runtime.Composable
 import wikipediacommons.modules.designsystem.generated.resources.Res
-import wikipediacommons.modules.designsystem.generated.resources.icon
+import wikipediacommons.modules.designsystem.generated.resources.logo
 import org.jetbrains.compose.resources.painterResource
 
 val defaultAppIcon
     @Composable
-    get() = painterResource(Res.drawable.icon)
+    get() = painterResource(Res.drawable.logo)
 
 // imageResource(Res.drawable.icon)
