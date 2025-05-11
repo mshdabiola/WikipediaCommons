fe8776a175ac8d4662b0fd4ba96fbd4e8a7a9b23
mshdabiola <mshdabiola@gmail.com>
2025-05-11 09:37:41 +0100

    Rename hydraulic to wcs

--- modules/ui/src/main/kotlin/com/mshdabiola/ui/ProfileCard.android.kt
+++ modules/ui/src/main/kotlin/com/mshdabiola/ui/ProfileCard.android.kt
@@ -2,13 +2,13 @@ package com.mshdabiola.ui
 
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import com.mshdabiola.designsystem.theme.HyaTheme
+import com.mshdabiola.designsystem.theme.WcsTheme
 
 @DevicePreviews
 @ThemePreviews
 @Composable
 fun ProfileCardPreview() {
-    HyaTheme {
+    WcsTheme {
         Surface {
             ProfileCard()
         }
