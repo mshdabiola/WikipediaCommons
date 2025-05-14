3148ee56a72bb27be56f7902763f43fd601970ce
mshdabiola <mshdabiola@gmail.com>
2025-05-14 21:26:06 +0100

    feat: Implement Note repository
    This commit introduces a `NoteRepository` to handle data operations related to notes.
    
    - Adds `NoteEx.kt` with extension functions to convert between `Note` model and `NoteEntity` database types.
    - Implements `RealModelRepository` as a concrete implementation of `NoteRepository`, utilizing `NoteDao` for database interactions.
    - Includes functions for upserting, retrieving all, retrieving a single, and deleting notes.
    - Configures dependency injection in `DataModule.kt` to provide `RealModelRepository` as the `NoteRepository`.

--- modules/data/src/commonMain/kotlin/com/mshdabiola/data/di/DataModule.kt
+++ modules/data/src/commonMain/kotlin/com/mshdabiola/data/di/DataModule.kt
@@ -2,28 +2,26 @@ package com.mshdabiola.data.di
 
 import com.mshdabiola.analytics.di.analyticsModule
 import com.mshdabiola.data.repository.INetworkRepository
+import com.mshdabiola.data.repository.NoteRepository
 import com.mshdabiola.data.repository.OfflineFirstUserDataRepository
 import com.mshdabiola.data.repository.RealINetworkRepository
+import com.mshdabiola.data.repository.RealModelRepository
 import com.mshdabiola.data.repository.UserDataRepository
+import com.mshdabiola.database.di.databaseModule
 import com.mshdabiola.datastore.di.datastoreModule
 import com.mshdabiola.network.di.networkModule
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
-expect val dataModule: Module
-//    module {
-//    includes(datastoreModule, databaseModule, networkModule, analyticsModule)
-//    single { Dispatchers.IO } bind CoroutineDispatcher::class
-//    singleOf(::RealINetworkRepository) bind INetworkRepository::class
-//    singleOf(::RealModelRepository) bind NoteRepository::class
-//    singleOf(::OfflineFirstUserDataRepository) bind UserDataRepository::class
-// }
-
-val commonModule =
-    module {
-        includes(datastoreModule, networkModule, analyticsModule)
-        singleOf(::RealINetworkRepository) bind INetworkRepository::class
-        singleOf(::OfflineFirstUserDataRepository) bind UserDataRepository::class
-    }
+ val dataModule=
+     module {
+         includes(databaseModule,datastoreModule, networkModule, analyticsModule)
+         singleOf(::RealINetworkRepository) bind INetworkRepository::class
+         singleOf(::OfflineFirstUserDataRepository) bind UserDataRepository::class
+         single { Dispatchers.IO } bind CoroutineDispatcher::class
+         singleOf(::RealModelRepository) bind NoteRepository::class
+     }
