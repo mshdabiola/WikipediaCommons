f40e19e51f4c9d1f8c8c11f42a80763879e02546
mshdabiola <mshdabiola@gmail.com>
2025-05-22 13:17:50 +0100

    feat: Add Image and MainImage data classes
    This commit introduces two new data classes:
    
    - `Image`: Represents detailed information about an image, including its description URLs, media type, MIME type, timestamp, URL, user, user ID, and a unique ID.
    - `MainImage`: Represents core information about an image, including its canonical title, description URLs, MIME type, URL, and user.

---
+++ modules/model/src/commonMain/kotlin/com/mshdabiola/model/MainImage.kt
@@ -0,0 +1,11 @@
+package com.mshdabiola.model
+
+
+data class MainImage(
+    val canonicaltitle: String,
+    val descriptionshorturl: String,
+    val descriptionurl: String,
+    val mime: String,
+    val url: String,
+    val user: String
+)
