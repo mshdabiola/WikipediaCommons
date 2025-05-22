6e9a6a71fc74b645ea345a17e49a0a335401333d
mshdabiola <mshdabiola@gmail.com>
2025-05-22 13:30:10 +0100

    feat: Add `toMainImage()` extension function for `Imageinfo`
    This commit introduces a new extension function `toMainImage()` to the `Imageinfo` class. This function maps properties from an `Imageinfo` object to a `MainImage` object, facilitating the conversion between these two types.

--- modules/data/src/nonJsMain/kotlin/com/mshdabiola/data/model/NoteEx.kt
+++ modules/data/src/nonJsMain/kotlin/com/mshdabiola/data/model/NoteEx.kt
@@ -5,8 +5,19 @@
 package com.mshdabiola.data.model
 
 import com.mshdabiola.database.model.NoteEntity
+import com.mshdabiola.model.MainImage
 import com.mshdabiola.model.Note
+import com.mshdabiola.network.model.Imageinfo
 
 fun Note.asNoteEntity() = NoteEntity(if (id == -1L) null else id, title, content)
 
 fun NoteEntity.asNote() = Note(id ?: -1, title, content)
+
+fun Imageinfo.toMainImage() = MainImage(
+    canonicaltitle,
+    descriptionshorturl,
+    descriptionurl,
+    mime,
+    url,
+    user
+)
