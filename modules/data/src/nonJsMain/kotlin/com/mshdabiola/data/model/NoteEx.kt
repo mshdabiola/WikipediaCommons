33a8bb09ce72c6e4cd63ea5c0bdf23ee1b83f1c6
mshdabiola <mshdabiola@gmail.com>
2025-05-24 12:20:59 +0100

    feat: Add MainImage to Entity mapping
    This commit introduces mapping functions to convert between the `MainImage` data model and the `MainImageEntity` database entity.
    
    Specifically:
    - `MainImage.asMainImageEntity(page: Int)`: Converts a `MainImage` object to a `MainImageEntity`, including a `page` parameter.
    - `MainImageEntity.asMainImage()`: Converts a `MainImageEntity` object back to a `MainImage` data model.

--- modules/data/src/nonJsMain/kotlin/com/mshdabiola/data/model/NoteEx.kt
+++ modules/data/src/nonJsMain/kotlin/com/mshdabiola/data/model/NoteEx.kt
@@ -4,6 +4,7 @@
 
 package com.mshdabiola.data.model
 
+import com.mshdabiola.database.model.MainImageEntity
 import com.mshdabiola.database.model.NoteEntity
 import com.mshdabiola.model.MainImage
 import com.mshdabiola.model.Note
@@ -11,3 +12,20 @@ import com.mshdabiola.model.Note
 fun Note.asNoteEntity() = NoteEntity(if (id == -1L) null else id, title, content)
 
 fun NoteEntity.asNote() = Note(id ?: -1, title, content)
+
+fun MainImage.asMainImageEntity(page:Int) = MainImageEntity(
+    title = title,
+    mime = mime,
+    sha1 = sha1,
+    url = url,
+    user = user,
+    page=page
+)
+
+fun MainImageEntity.asMainImage() = MainImage(
+    title = title,
+    mime = mime,
+    sha1 = sha1,
+    url = url,
+    user = user
+)
