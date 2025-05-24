b4f8642fb7460a624e70efafa1c7dcdc4d4cc253
mshdabiola <mshdabiola@gmail.com>
2025-05-24 11:56:27 +0100

    Refactor: Simplify RealMediaRepository and remove unused dependencies
    This commit refactors `RealMediaRepository` by:
    
    *   Removing the unused `INetworkDataSource` dependency.
    *   Moving the mapping logic from network response models to `MainImage` out of the repository. The `mediaDataSource.getAllImages()` method is now expected to return a `List<MainImage>` directly.
    *   Removing the `Imageinfo.toMainImage()` extension function as it's no longer used within the data module.

--- modules/data/src/nonJsMain/kotlin/com/mshdabiola/data/model/NoteEx.kt
+++ modules/data/src/nonJsMain/kotlin/com/mshdabiola/data/model/NoteEx.kt
@@ -7,18 +7,7 @@ package com.mshdabiola.data.model
 import com.mshdabiola.database.model.NoteEntity
 import com.mshdabiola.model.MainImage
 import com.mshdabiola.model.Note
-import com.mshdabiola.network.model.Imageinfo
 
 fun Note.asNoteEntity() = NoteEntity(if (id == -1L) null else id, title, content)
 
 fun NoteEntity.asNote() = Note(id ?: -1, title, content)
-
-fun Imageinfo.toMainImage() = MainImage(
-    canonicaltitle,
-    descriptionshorturl,
-    descriptionurl,
-    mime,
-    sha1,
-    url,
-    user
-)
--- modules/data/src/nonJsMain/kotlin/com/mshdabiola/data/repository/RealMediaRepository.kt
+++ modules/data/src/nonJsMain/kotlin/com/mshdabiola/data/repository/RealMediaRepository.kt
@@ -1,15 +1,12 @@
 package com.mshdabiola.data.repository
 
-import com.mshdabiola.data.model.toMainImage
 import com.mshdabiola.model.MainImage
 import com.mshdabiola.network.IMediaDataSource
-import com.mshdabiola.network.INetworkDataSource
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 
 class RealMediaRepository(
     private val mediaDataSource: IMediaDataSource,
-    private val networkData : INetworkDataSource,
     private val ioDispatcher: CoroutineDispatcher,
 ) : IMediaRepository{
     override suspend fun getAllMedia(
@@ -18,9 +15,7 @@ class RealMediaRepository(
     ): List<MainImage> {
        return withContext(ioDispatcher){
             mediaDataSource.getAllImages(limit)
-                .mapNotNull { it?.imageinfo}
-                .flatten()
-                .mapNotNull { it?.toMainImage() }
+
 
         }
     }
