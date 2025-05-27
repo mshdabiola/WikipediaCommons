c133637cc700ae023da74121db532f640ec660fd
mshdabiola <mshdabiola@gmail.com>
2025-05-27 21:30:51 +0100

    Test: Implement FakeMediaRepository
    This commit provides a basic implementation for `FakeMediaRepository`.
    
    - `bookmarkSet`: Now returns a `flowOf(emptySet())`.
    - `getAllMedia`: Now returns an empty list.
    - `toggleBookmark`: The function body is now empty (previously threw `NotImplementedError`).

--- modules/testing/src/commonMain/kotlin/com/mshdabiola/testing/fake/repository/FakeMediaRepository.kt
+++ modules/testing/src/commonMain/kotlin/com/mshdabiola/testing/fake/repository/FakeMediaRepository.kt
@@ -3,19 +3,19 @@ package com.mshdabiola.testing.fake.repository
 import com.mshdabiola.data.repository.IMediaRepository
 import com.mshdabiola.model.MainImage
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 
 class FakeMediaRepository : IMediaRepository {
     override val bookmarkSet: Flow<Set<String>>
-        get() = TODO("Not yet implemented")
+        get() = flowOf(emptySet())
 
     override suspend fun getAllMedia(
         page: Int,
         limit: Int,
     ): List<MainImage> {
-        TODO("Not yet implemented")
+        return emptyList()
     }
 
     override suspend fun toggleBookmark(id: String) {
-        TODO("Not yet implemented")
     }
 }
