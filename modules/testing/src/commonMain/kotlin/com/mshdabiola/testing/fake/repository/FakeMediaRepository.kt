15928e4163c6a7bfbf3c87d3a09978db2a751bc2
mshdabiola <mshdabiola@gmail.com>
2025-05-27 12:57:58 +0100

    Test: Update FakeMediaRepository interface
    This commit updates the `FakeMediaRepository` to fully implement the `IMediaRepository` interface.
    
    Specifically, it adds stubs for the following members:
    - `bookmarkSet: Flow<Set<String>>`
    - `toggleBookmark(id: String)`

--- modules/testing/src/commonMain/kotlin/com/mshdabiola/testing/fake/repository/FakeMediaRepository.kt
+++ modules/testing/src/commonMain/kotlin/com/mshdabiola/testing/fake/repository/FakeMediaRepository.kt
@@ -2,12 +2,20 @@ package com.mshdabiola.testing.fake.repository
 
 import com.mshdabiola.data.repository.IMediaRepository
 import com.mshdabiola.model.MainImage
+import kotlinx.coroutines.flow.Flow
 
 class FakeMediaRepository : IMediaRepository {
+    override val bookmarkSet: Flow<Set<String>>
+        get() = TODO("Not yet implemented")
+
     override suspend fun getAllMedia(
         page: Int,
         limit: Int,
     ): List<MainImage> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun toggleBookmark(id: String) {
         TODO("Not yet implemented")
     }
 }
