4588474f5f6643db5c82a177b3ca24aaf1d6e880
mshdabiola <mshdabiola@gmail.com>
2025-05-28 12:58:53 +0100

    Refactor: Use CenterAlignedTopAppBar in MainScreen
    This commit replaces the `TopAppBar` with `CenterAlignedTopAppBar` in the `MainScreen` composable.
    
    This change centers the title within the app bar.

--- features/main/src/commonMain/kotlin/com/mshdabiola/main/MainScreen.kt
+++ features/main/src/commonMain/kotlin/com/mshdabiola/main/MainScreen.kt
@@ -19,19 +19,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.filled.Bookmark
-import androidx.compose.material.icons.filled.BookmarkBorder
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -121,7 +117,7 @@ internal fun MainScreen(
                 .fillMaxSize()
                 .testTag("main:root"),
     ) {
-        TopAppBar(
+        CenterAlignedTopAppBar(
             title = { Text("Wikipedia Commons") },
             navigationIcon = {
                 IconButton(onClick = onMenuClick) {
