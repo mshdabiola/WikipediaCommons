e6ceee8a6ff32d8f0a4fd0853cbb898209d91807
mshdabiola <mshdabiola@gmail.com>
2025-05-30 17:07:34 +0100

    Refactor: Rename Platform.jvm.kt and update generalPath
    This commit renames `Platform.jvm.kt` to `JVMPlatform.jvm.kt`.
    
    Additionally, the `generalPath` for desktop in `GeneralPath.desktop.kt` has been updated from `System.getProperty("java.io.tmpdir") + "/hydraulics"` to `System.getProperty("java.io.tmpdir") + "/wikipediacommons"`.

--- modules/model/src/jvmMain/kotlin/com/mshdabiola/model/GeneralPath.desktop.kt
+++ modules/model/src/jvmMain/kotlin/com/mshdabiola/model/GeneralPath.desktop.kt
@@ -1,4 +1,5 @@
 package com.mshdabiola.model
 
 actual val generalPath: String
-    get() = System.getProperty("java.io.tmpdir") + "/hydraulics" // "${System.getProperty("user.home")}/AppData/Local/wikipediacommons"
+    get() = System.getProperty("java.io.tmpdir") + "/wikipediacommons"
+// "${System.getProperty("user.home")}/AppData/Local/wikipediacommons"
