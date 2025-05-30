43536f07cdfa8d66cf5d9a25c0d48e8b028661c6
mshdabiola <mshdabiola@gmail.com>
2025-05-30 17:07:22 +0100

    Refactor: Improve error handling and remove unused files
    This commit introduces the following changes:
    
    - **Error Handling:**
        - Replaced generic `Exception` catches in `MediaDataSource.kt` with `kotlinx.io.IOException`.
        - Introduced a custom `NetworkDataSourceException` to wrap network-related errors, providing more specific error context.
    - **File Removal:**
        - Deleted the unused `Articles.kt`, `Wiki.kt`, and `WikiAPI.kt` files from the `com.mshdabiola.network.request` package.

---
+++ modules/network/src/commonMain/kotlin/com/mshdabiola/network/NetworkDataSourceException.kt
@@ -0,0 +1,6 @@
+package com.mshdabiola.network
+
+class NetworkDataSourceException(
+    message: String,
+    cause: Throwable? = null,
+) : Exception(message, cause)
--- modules/network/src/commonMain/kotlin/com/mshdabiola/network/request/Wiki.kt
+++
@@ -1,6 +0,0 @@
-package com.mshdabiola.network.request
-
-import io.ktor.resources.Resource
-
-@Resource("")
-class Wiki
