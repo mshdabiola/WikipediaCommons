9d89586e89cdca8058b8fdd36a2afb6a92fea0e7
mshdabiola <mshdabiola@gmail.com>
2025-06-05 00:27:44 +0100

    Test: Update SearchScreenTest with initial search state
    This commit updates the `SearchScreenTest` to initialize the `SearchScreen` with a `SearchState.Loading` and an empty `rememberTextFieldState()` for `searchQuery`.
    
    This change ensures that the test for `SearchScreen` starts with a defined initial state for search operations.

--- features/search/src/jvmTest/kotlin/com/mshdabiola/search/SearchScreenTest.kt
+++ features/search/src/jvmTest/kotlin/com/mshdabiola/search/SearchScreenTest.kt
@@ -2,6 +2,7 @@ package com.mshdabiola.search
 
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.junit4.createComposeRule
 import com.mshdabiola.ui.SharedContentPreview
@@ -21,6 +22,8 @@ class SearchScreenTest {
                     modifier = Modifier.fillMaxSize(),
                     sharedTransitionScope = sharedTransitionScope,
                     animatedContentScope = animatedContentScope,
+                    searchState = SearchState.Loading,
+                    searchQuery = rememberTextFieldState(),
                 )
             }
         }
